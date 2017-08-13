2e506b907fbb44db6c9f5954fd30470e73bb6c25
Leandro Gomez <leandrogonqn@hotmail.com>
2017-08-13 15:44:32 -0300

    #65 abm terminado

---
+++ dom/src/main/java/domainapp/dom/riesgoAP/RiesgosAPMenu.java
@@ -0,0 +1,108 @@
+package domainapp.dom.riesgoAP;
+
+import java.util.Date;
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.InvokeOn;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
+import domainapp.dom.cliente.Clientes;
+import domainapp.dom.cliente.ClientesRepository;
+import domainapp.dom.compania.CompaniaRepository;
+import domainapp.dom.compania.Companias;
+import domainapp.dom.detalleTipoPago.DetalleTipoPagos;
+import domainapp.dom.detalleTipoPago.DetalleTipoPagosRepository;
+import domainapp.dom.ocupacion.Ocupaciones;
+import domainapp.dom.ocupacion.OcupacionesRepository;
+import domainapp.dom.tipoTitular.TipoTitulares;
+import domainapp.dom.tipoTitular.TipoTitularesRepository;
+import domainapp.dom.tipoVivienda.TiposVivienda;
+import domainapp.dom.tipoVivienda.TiposViviendaRepository;
+import domainapp.dom.tiposDeCoberturas.TiposDeCoberturasRepository;
+
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        repositoryFor = RiesgosAP.class
+)
+@DomainServiceLayout(
+        named = "Polizas",
+        menuOrder = "1.3"
+)
+public class RiesgosAPMenu {
+	
+	  @Action(semantics = SemanticsOf.SAFE)
+	  @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT, named="Listar Polizas AP")
+	  @MemberOrder(sequence = "2")
+	  public List<RiesgosAP> listar() {
+			  List<RiesgosAP> listaPolizaRiesgoAP = polizasRepository.listar();
+			  for(int i=0; i< listaPolizaRiesgoAP.size(); i++) {
+				  listaPolizaRiesgoAP.get(i).actualizarPoliza();
+		        }
+		      return listaPolizaRiesgoAP;
+	    }
+	  
+	    public List<Clientes> choices1Crear(){
+	    	return clientesRepository.listarActivos();
+	    }
+	    
+	    public List<Companias> choices2Crear(){
+	    	return companiaRepository.listarActivos();
+	    }	    
+	    
+	    public List<DetalleTipoPagos> choices6Crear(){
+	    	return detalleTipoPagosRepository.listarActivos();
+	    }
+
+	    public static class CreateDomainEvent extends ActionDomainEvent<RiesgosAPMenu> {}
+	    @Action(domainEvent = CreateDomainEvent.class, invokeOn=InvokeOn.OBJECT_ONLY)
+	    @ActionLayout(named="Crear Poliza AP")
+	    @MemberOrder(sequence = "1")
+	    public RiesgosAP crear(
+/*0*/	            @ParameterLayout(named="Número") final String polizaNumero,
+/*1*/	            @ParameterLayout(named="Cliente") final Clientes polizaCliente,
+/*2*/	            @ParameterLayout(named="Compañia") final Companias polizaCompania,
+/*7*/	            @ParameterLayout(named="Fecha Emision") final Date polizaFechaEmision,
+/*8*/				@ParameterLayout(named="Fecha Vigencia") final Date polizaFechaVigencia,
+/*9*/				@ParameterLayout(named="Fecha Vencimiento") final Date polizaFechaVencimiento,
+/*11*/				@ParameterLayout(named="Pago") final DetalleTipoPagos polizaPago,
+/*13*/				@ParameterLayout(named="Precio Total") final double polizaImporteTotal,
+	    			@ParameterLayout(named="Muerte") final float riesgoAPMuerte,
+	    			@ParameterLayout(named="Invalidez") final float riesgoAPInvalidez,
+	    			@ParameterLayout(named="AMF")final float riesgoAPAMF)
+	    {
+	        return polizasRepository.crear(
+	        		polizaNumero,
+	        		polizaCliente,
+	        		polizaCompania,
+	        		polizaFechaEmision,
+	        		polizaFechaVigencia, 
+	        		polizaFechaVencimiento,
+	        		polizaPago,
+	        		polizaImporteTotal,
+	        		riesgoAPMuerte,
+	        		riesgoAPInvalidez,
+	        		riesgoAPAMF);
+	    }
+
+
+	    @javax.inject.Inject
+	    RiesgosAPRepository polizasRepository;
+	    @javax.inject.Inject
+	    ClientesRepository clientesRepository;
+	    @Inject
+	    DetalleTipoPagosRepository detalleTipoPagosRepository;
+	    @Inject
+	    CompaniaRepository companiaRepository;
+	    @Inject
+	    TiposDeCoberturasRepository tiposDeCoberturasRepository;
+
+}
